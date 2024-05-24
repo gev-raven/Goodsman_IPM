a2d7e721154261ada58750834334f18b5cd15d6a
gev-raven <gveenstra.ak@gmail.com>
2024-05-24 09:13:49 -0700

    added container folder

---
+++ Egg Modeling Test/Egg_Modeling_Test1.R
@@ -0,0 +1,9 @@
+# Uses code modified from Goodsman 2018 R code "IMAPfunctionNewJ2v2.R". This
+# code attempts to simulate chinook salmon egg development and
+# emergence timing.
+
+# Development rate equation from Abbie and Beacham and Murray Model
+# Temperature data for Upper Middle Fork Salmon River at Marsh Creek.
+
+# Model assumes no egg mortality.
+
